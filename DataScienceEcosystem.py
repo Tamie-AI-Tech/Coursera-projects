9cc44e800aac29399131baab163f822b1b910c4f
TAMIRIRAISHE NYEVERA <taminyevera@gmail.com>
2023-04-04 03:43:10 -0700

    Add files via upload
    Tools data science week 6, project. its all about familiarisation with the jupyter notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. MATLAB
+# 5. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. NumPy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Scikit-learn
+# 5. TensorFlow
+
+# =| Data Science Tools |
+# | ------------------ |
+# | Jupyter Notebook   |
+# | RStudio            |
+# | Spyder             |
+# 
+
+# ## Below are a few examples of evaluating arithmetic expressions in Python.
+
+# - a = 2
+# - b = 3
+# - c = 4
+# - d = (a * b) + c
+# - print(d)  # Output: 10
+# 
+
+# In[2]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3*4) + 5
+print(result)  # Output: 17
+
+
+#In this code, we use parentheses to control the order of evaluation and ensure that the multiplication is performed before the addition. 
+#The comment line explains the purpose of the code and makes it easier for other developers to understand what the code is doing. 
+#When you run this code cell, 
+#it should output the result 17, which is the expected value of the expression.
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+print(hours)  # Output: 3.3333333333333335
+
+#In this code, we use the division operator (/) to convert the number of minutes to hours.
+#The comment line explains the purpose of the code and makes it easier for other developers to understand what the code is doing. 
+#When you run this code cell, it should output the result 3.3333333333333335, which is the expected value of 200 minutes in hours.
+
+
+# **Objectives:**
+# 
+# - Learn how to create a project in Watson Studio
+# - Understand how to use Jupyter notebooks to write and run code
+# - Explore common data science languages, libraries, and tools
+# - Practice writing basic arithmetic expressions in Python
+# - Convert units of measurement using Python
+# - Use Git integration to collaborate on projects
+
+# ## Author
+# 
+# My name is [NYEVERA TAMIRIRAISHE].
+# 
+
+# In[ ]:
+
+
+
+
